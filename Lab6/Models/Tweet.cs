5f9dedb3986408ff020159e85418c83c98e400ca
Ababiya <47339223+AbabiyaAbajo@users.noreply.github.com>
2019-06-06 19:32:23 -0400

    Add files via upload

---
+++ Lab6/Models/Tweet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+
+namespace Lab6.Models
+{
+    public class Tweet
+    {
+        /// <summary>
+        /// Database id of the tweet
+        /// </summary>
+        public int TweetId
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "Username")]
+        /// <summary>
+        /// The person who created the tweet
+        /// </summary>
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "Content")]
+        /// <summary>
+        /// The content of the tweet
+        /// </summary>
+        public string Content
+        {
+            get;
+            set;
+        }
+    }
+}
