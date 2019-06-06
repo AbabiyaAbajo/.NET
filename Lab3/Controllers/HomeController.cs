f888a993df562d9f2baeb63c1712d5a08b7f9dd8
Ababiya <47339223+AbabiyaAbajo@users.noreply.github.com>
2019-06-06 19:22:51 -0400

    Add files via upload

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Lab3.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayForm(LoginInformationModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Lab3/Models/LoginInformationModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Controllers
+{
+    public class LoginInformationModel
+    {
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string fname
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string lname
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(3)]
+        [EmailAddress]
+        public string age
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string emailadd
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(10)]
+        [EmailAddress]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime dob
+        {
+            get { return dob; }
+            set { dob = value; }
+        }
+
+        [Required]
+        [StringLength(100)]
+        public string Password
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string description
+        {
+            get;
+            set;
+        }
+    }
+}
